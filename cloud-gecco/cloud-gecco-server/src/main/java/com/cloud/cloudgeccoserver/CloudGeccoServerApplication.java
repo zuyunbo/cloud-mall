dbf65429a6941ba98f6814c0de02f9d461504c97
祖云波 <460895202@qq.com>
2021-03-16 14:53:02 +0800

    第一版测试封装{测试封装新增save接口完成}

---
+++ cloud-gecco/cloud-gecco-server/src/main/java/com/cloud/cloudgeccoserver/CloudGeccoServerApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudgeccoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGeccoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGeccoServerApplication.class, args);
+    }
+
+}
---
+++ cloud-gecco/cloud-gecco-server/src/test/java/com/cloud/cloudgeccoserver/CloudGeccoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudgeccoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGeccoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
