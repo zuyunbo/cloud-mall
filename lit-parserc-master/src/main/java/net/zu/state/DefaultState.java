44f0b5727acfa3ded1cd102cb4878bcb4ea0c756
祖云波 <460895202@qq.com>
2021-04-22 10:14:54 +0800

    第一版测试封装{测试封装新增save接口完成}

---
+++ lit-parserc-master/src/main/java/net/zu/state/DefaultState.java
@@ -0,0 +1,50 @@
+package net.zu.state;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * @Author zuyunbo
+ * @Date 2021/4/20  1:55 下午
+ **/
+public class DefaultState<T> implements State<T>{
+
+
+    private final T state;
+
+    private final Function<T,T> next;
+
+    private final Predicate<T> terminated;
+
+    public DefaultState(T state, Function<T, T> next, Predicate<T> terminated) {
+        this.state = state;
+        this.next = next;
+        this.terminated = terminated;
+    }
+
+    @Override
+    public T get() {
+        return state;
+    }
+
+    @Override
+    public State<T> next() {
+        return new DefaultState<>(next.apply(state),next,terminated);
+    }
+
+    @Override
+    public State<T> next(int n) {
+        State<T> next = this;
+        for (int i = 0; i < n; i++) {
+            next = this.next();
+            if (next.terminated())
+                return null;
+        }
+        return next;
+    }
+
+    @Override
+    public boolean terminated() {
+        return terminated.test(this.state);
+    }
+}
