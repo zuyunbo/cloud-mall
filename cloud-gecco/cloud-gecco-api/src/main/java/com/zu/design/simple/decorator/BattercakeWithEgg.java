c69f0718dbd998e76a1cd2e9e0ea08bc09b40996
祖云波 <460895202@qq.com>
2021-05-12 11:30:28 +0800

    第一版测试封装{测试封装新增save接口完成}

--- cloud-gecco/cloud-gecco-api/src/main/java/com/zu/design/simple/decorator/BattercakeWithEgg.java
+++ cloud-gecco/cloud-gecco-api/src/main/java/com/zu/design/simple/decorator/BattercakeWithEgg.java
@@ -16,4 +16,14 @@ public class BattercakeWithEgg extends Battercake{
           return super.getPrice()+1;
       }
 
+
+    public static void main(String[] args) {
+
+          String s = "005056B089C11EEBA48DD085FE73D9C9_210329CDX95";
+        String substring = s.substring(0, 32);
+        System.out.println(substring);
+
+
+    }
+
 }
